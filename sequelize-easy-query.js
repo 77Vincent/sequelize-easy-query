7ba277d3e4d9ab6c68d61b686a366646b8e8397e
Wen <vwen@apac.adp.com>
2018-07-06 16:05:29 +0800

    support multiple select

--- sequelize-easy-query.js
+++ sequelize-easy-query.js
@@ -3,35 +3,24 @@ const querystring = require('querystring')
 
 const { Op } = Sequelize
 
+// Operators
+const or = Op ? Op.or : '$or'
+const like = Op ? Op.like : '$like'
+
+/**
+ * Get type
+ */
 const is = input => Object.prototype.toString.call(input)
 
-const isEmpty = input => input !== undefined && input !== null && input !== ''
-
-const processAlias = (alias = {}, inputQueryObject = {}) => {
-  const outputQueryObject = Object.assign({}, inputQueryObject)
-  Object.keys(alias).map((key) => {
-    const newKey = alias[key]
-    if (newKey === key) {
-      return null
-    }
-    // Remove the origin property
-    if (Object.prototype.hasOwnProperty.call(outputQueryObject, key)) {
-      delete outputQueryObject[key]
-    }
-    // Add a new property using alias and give it origin value
-    if (Object.prototype.hasOwnProperty.call(outputQueryObject, newKey)) {
-      outputQueryObject[key] = outputQueryObject[newKey]
-      delete outputQueryObject[newKey]
-    }
-    return null
-  })
-  return outputQueryObject
-}
+/**
+ * If a variable has meaningful value
+ */
+const hasValue = input => input !== undefined && input !== null && input !== ''
 
 /**
  * Generate query object for the sequelize "where" or "order" clauses
  */
-module.exports = (rawQuerystring = '', options = { jjj: [] }) => {
+module.exports = (rawQuerystring = '', options = {}) => {
   if (is(options) !== '[object Object]') {
     throw new Error('The second parameter: options should be type of Object')
   }
@@ -49,8 +38,30 @@ module.exports = (rawQuerystring = '', options = { jjj: [] }) => {
   // Initial the output object
   const output = order || orderBy || orderByAlias ? [] : {}
 
-  const queryObject = processAlias(filterByAlias || orderByAlias, querystring
-    .parse(`${rawQuerystring}&${querystring.stringify(filter || order)}`))
+  // Get full query string with pre-query values
+  const fullQuerystring =
+    `${rawQuerystring}&${querystring.stringify(filter || order)}&${querystring.stringify({ search })}`
+
+  // Add pre-query values into query object
+  const queryObject = querystring.parse(fullQuerystring)
+  const alias = filterByAlias || orderByAlias || {}
+
+  Object.keys(alias).map((key) => {
+    const newKey = alias[key]
+    if (newKey === key) {
+      return null
+    }
+    // Remove the origin property
+    if (Object.prototype.hasOwnProperty.call(queryObject, key)) {
+      delete queryObject[key]
+    }
+    // Add a new property using alias and give it origin value
+    if (Object.prototype.hasOwnProperty.call(queryObject, newKey)) {
+      queryObject[key] = queryObject[newKey]
+      delete queryObject[newKey]
+    }
+    return null
+  })
 
   // Default values
   filter = filter || {}
@@ -61,31 +72,22 @@ module.exports = (rawQuerystring = '', options = { jjj: [] }) => {
   orderBy = orderBy || []
   orderByAlias = orderByAlias || {}
 
-  search = search || null
+  search = search || []
   searchBy = searchBy || []
 
-
-  if (search) {
-    queryObject.search = search
-  }
-
+  // Add keys defined in pre-query and queryByAlias into queryBy
   Object.keys(filter).map(key => filterBy.push(key))
-  Object.keys(filterByAlias).map(key => filterBy.push(key))
   Object.keys(order).map(key => orderBy.push(key))
+  Object.keys(filterByAlias).map(key => filterBy.push(key))
   Object.keys(orderByAlias).map(key => orderBy.push(key))
 
+  // From here are the actual basic query
   // Filter by
   Object.keys(queryObject).map((key) => {
     if (filterBy.indexOf(key) !== -1 && key !== 'search') {
       const queryValue = queryObject[key]
-      if (isEmpty(queryValue)) {
-        switch (is(queryValue)) {
-          case '[object Array]':
-            output[key] = decodeURI(queryValue).split(',').filter(value => isEmpty(value))
-            break
-          default:
-            output[key] = decodeURI(queryValue)
-        }
+      if (hasValue(queryValue)) {
+        output[key] = String(queryValue).split(',').filter(value => hasValue(value))
       }
     }
     return null
@@ -93,31 +95,29 @@ module.exports = (rawQuerystring = '', options = { jjj: [] }) => {
 
   // Search by
   if (searchBy.length && queryObject.search) {
-    const arrayForSearch = searchBy.map(value => ({
-      [value]: {
-        [(Op ? Op.like : '$like')]: `%${decodeURI(queryObject.search)}%`,
+    const arrayForSearch = searchBy.map(column => ({
+      [column]: {
+        [or]: String(queryObject.search)
+          .split(',')
+          .filter(value => hasValue(value))
+          .map(value => ({ [like]: `%${value}%` })),
       },
     }))
-    output[(Op ? Op.or : '$or')] = arrayForSearch
+    output[or] = arrayForSearch
   }
 
   // Order by
   Object.keys(queryObject).map((key) => {
     if (orderBy.indexOf(key) !== -1) {
       const queryValue = queryObject[key]
-      if (isEmpty(queryValue)) {
-        switch (is(queryValue)) {
-          case '[object Array]':
-            output.push([key, decodeURI(queryValue).split(',')[0]])
-            break
-          default:
-            output.push([key, decodeURI(queryValue)])
-        }
+      if (hasValue(queryValue)) {
+        output.push([key, String(queryValue).split(',')[0]])
       }
     }
     return null
   })
 
-  if (!Object.keys(output).length && !output[(Op ? Op.or : '$or')]) { return null }
+  // Only return when it has query object
+  if (!Object.keys(output).length && !output[or]) { return null }
   return output
 }
